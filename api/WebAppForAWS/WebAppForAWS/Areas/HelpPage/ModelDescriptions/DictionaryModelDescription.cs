bc391eeb09ce4fe1aa96216de967c09b096fe435
Kosuke Nakamura <nakamura.kosuke@opst.jp>
2017-08-09 10:52:15 +0900

    webAppSample追加

---
+++ api/WebAppForAWS/WebAppForAWS/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAppForAWS.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ api/WebAppForAWS/WebAppForAWS/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppForAWS.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}

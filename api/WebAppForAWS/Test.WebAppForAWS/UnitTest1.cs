0b41b8e47b2a8ddbeedeac959f7dda8a63e6672d
Kosuke Nakamura <nakamura.kosuke@opst.jp>
2017-08-16 18:35:25 +0900

    ユニットテストを追加 不要なファイルを削除

---
+++ api/WebAppForAWS/Test.WebAppForAWS/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebAppForAWS.Controllers;
+using System.Web;
+
+namespace Test.WebAppForAWS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var valuesController = new ValuesController();
+
+            var result = valuesController.Get();
+
+
+            Assert.IsInstanceOfType(result, typeof(IEnumerable<string>));
+
+            var resulyArray = result.ToArray();
+
+            Assert.AreEqual("value1", resulyArray[0]);
+            Assert.AreEqual("value2", resulyArray[1]);
+            
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            var valuesController = new ValuesController();
+
+            var result = valuesController.Get(1);
+
+            Assert.IsInstanceOfType(result, typeof(string));
+            Assert.AreEqual("value", result);
+
+
+        }
+    }
+}
